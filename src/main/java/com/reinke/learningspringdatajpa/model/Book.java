b2049b1731436284a89ccfca211f554761433a3e
renias <reinke.krzysztof@o2.pl>
2021-03-29 13:45:01 +0200

    adding to book entity constructors, getters and setters

--- src/main/java/com/reinke/learningspringdatajpa/model/Book.java
+++ src/main/java/com/reinke/learningspringdatajpa/model/Book.java
@@ -35,5 +35,45 @@ public class Book {
     @Column(name = "created_on")
     private Timestamp createdAt;
 
+    public Book() {
+    }
 
+    public Book(Long id, Student student, String name, Timestamp createdAt) {
+        this.id = id;
+        this.student = student;
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
